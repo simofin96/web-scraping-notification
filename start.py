17a801eb0d6ad7d7bb5585474772c6dacd82ca2d
simofin96 <65085506+simofin96@users.noreply.github.com>
2024-05-29 22:33:11 +0200

    Improved code robustness

--- start.py
+++ start.py
@@ -1,49 +1,51 @@
 # Source: https://www.geeksforgeeks.org/python-script-to-monitor-website-changes/
 # Website to monitor: https://www.idays.it/tickets
 
-import hashlib 
+import hashlib
+import re
 import time
 import urllib.request
-from xxlimited import new
+
+
+def clean_data(data):
+	string_data = data.decode("utf-8")
+	cleaned_string_data = re.sub(r"<!--.*?-->", "", string_data) # delete HTML comments
+	cleaned_data = cleaned_string_data.encode("utf-8")
+	return cleaned_data
 
 # setting the URL you want to monitor
-url = "https://www.geeksforgeeks.org"
+url = "https://store.steampowered.com/?l=italian"
 
 # perform a GET request and load the content of the website
 page = urllib.request.urlopen(url)
 response = page.read()
+response = clean_data(response)
 
 # create the initial hash 
 currentHash = hashlib.sha224(response).hexdigest() 
 
 print("running") 
-time.sleep(10) 
 while True: 
+	time.sleep(2) # time waited between requests 
+
 	try:
-		# perform the get request and store it in a var 
+		# perform the get request
 		page = urllib.request.urlopen(url)
-		response = page.read() 
+		response = page.read()
+		response = clean_data(response) 
 
 		# create a hash 
 		newHash = hashlib.sha224(response).hexdigest()
 
-		# wait for 30 seconds 
-		time.sleep(30) 
-
 		# check if new hash is same as the previous hash 
 		if newHash == currentHash: 
+			print(currentHash)
 			continue
-
-		# if something changed in the hashes 
 		else: 
 			# notify 
-			print("something changed") 
-
-			# create a hash 
+			print("something changed")
+			print(currentHash)
 			currentHash = newHash
-
-			# wait for 30 seconds 
-			time.sleep(30) 
 			continue
 
 	except Exception as e: 
